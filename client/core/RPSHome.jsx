cf15d880af9f0ebe5ccb71013f4cfb9a3f8fffdd
Jorge Oviedo <jorgeos@gmail.com>
2023-11-18 15:33:01 -0500

    Added buttoms on the homepage

--- client/core/RPSHome.jsx
+++ client/core/RPSHome.jsx
@@ -4,9 +4,11 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import blueBackgroundImage from './../assets/images/Bgcolor.png';
 import quantumLogo from './../assets/images/quantumLogo.jpg';
 import homePageBG from './../assets/images/BigBg.png';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -37,6 +39,9 @@ const useStyles = makeStyles((theme) => ({
     color: '#fff',
     textAlign: 'center',
   },
+  button: {
+    margin: theme.spacing(3, 20, 3),
+  },
 }));
 
 export default function RPSHome() {
@@ -52,6 +57,33 @@ export default function RPSHome() {
         <Typography variant="body2" component="p" className={classes.content}>
           Experience the excitement of Rock Paper Scissors in our tournament. Compete, have fun, and emerge victorious!
         </Typography>
+        <Button
+          component={Link}
+          to="/signup"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Create User
+        </Button>
+        <Button
+          component={Link}
+          to="/match"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Create Match
+        </Button>
+        <Button
+          component={Link}
+          to="/rank"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          See Ranking
+        </Button>
       </CardContent>
     </Card>
   );
