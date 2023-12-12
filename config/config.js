e033d957cecc437259c5ab0dde9f71bb57fdfa84
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2023-12-12 13:38:10 -0500

    Relolad Fix #3

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 3000,
+  port: process.env.SERVER_PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
     process.env.MONGODB_URI ||
